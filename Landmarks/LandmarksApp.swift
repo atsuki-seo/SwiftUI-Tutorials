fd1483f6cd9156867663445b812a98081c454caf
atsuki.seo <atsuki.seo@mixi.co.jp>
2024-05-13 00:58:00 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by atsuki.seo on 2024/05/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
